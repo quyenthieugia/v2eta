c60ed8584d6f52b743aaebedb64622741913bd75
quyendn <quyendn84@gmail.com>
2024-07-13 23:45:07 +0700

    quyendn

---
+++ models/webhook.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        data = request.json
+        print(f"[>] data \"{data}\"...")
+        if data and 'ref' in data and data['ref'] == 'refs/heads/master':
+            subprocess.call(['/var/www/v2eta/v2eta/update_script.sh'])
+        return 'Webhook received', 200
+    else:
+        return 'Invalid request', 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
